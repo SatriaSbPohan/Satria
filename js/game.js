d4b991c57f9dd335972ebe4bb06b00ee9e4359e3
SatriaSbPohan <satriapohan12@gmail.com>
2022-08-09 09:44:08 -0500

    perbaikan link game

---
+++ js/game.js
@@ -0,0 +1,10 @@
+const satu = document.getElementsByClassName("satu")[0];
+const dua = document.getElementsByClassName("dua")[0];
+
+const button = document.getElementsByClassName("button")[0];
+
+button.addEventListener("click", jodoh);
+
+function jodoh () {
+    satu.text
+}
